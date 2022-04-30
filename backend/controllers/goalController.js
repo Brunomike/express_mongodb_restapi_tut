964cad1eaae05e0fabaaa732ea170ad059c450c4
Michael Bruno <brunomike254@gmail.com>
2022-04-30 19:37:28 +0300

    Added authorization to the api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 //@desc Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id });
     res.status(200).json(goals);
 });
 
@@ -18,6 +19,7 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field!');
     }
     const goal = await Goal.create({
+        user: req.user.id,
         text: req.body.text
     });
     res.status(200).json(goal);
@@ -32,6 +34,19 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Goal not found');
     }
+    const user = await User.findById(req.user.id);
+
+    //Check for user
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    //Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized');
+    }
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.status(200).json(updatedGoal);
 });
@@ -45,6 +60,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Goal not found');
     }
+
+    const user = await User.findById(req.user.id);
+
+    //Check for user
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    //Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized');
+    }
+
     await goal.remove();
     res.status(200).json({ id: req.params.id });
 });
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
         text: {
             type: String,
             required: [true, 'Please add a text value'],
@@ -9,7 +14,7 @@ const goalSchema = mongoose.Schema(
     },
     {
         timestamps: true
-    } 
+    }
 );
 
-module.exports=mongoose.model('Goal',goalSchema);
+module.exports = mongoose.model('Goal', goalSchema);
