9c9c7d868f285b2048d1999834448c8f12c891bc
Michael Bruno <brunomike254@gmail.com>
2022-04-30 13:44:05 +0300

    Goals controller, routes set up and overriden the default express error-handler

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController');
+
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
+
+// router.get('/', getGoals);
+// router.post('/', setGoal);
+// router.put('/:id', updateGoal);
+// router.delete('/:id', deleteGoal);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
